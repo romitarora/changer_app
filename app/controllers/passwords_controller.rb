1cb792dfb02433deab060ab152db21c6cdb5722f
kinjal Babariya <kinjal@itoneclick.com>
2017-07-31 16:58:07 +0530

    add new functionality54

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,5 +1,7 @@
 class PasswordsController < Devise::PasswordsController 
-   
+   prepend_before_action :require_no_authentication
+  # Render the #edit only if coming from a reset password email link
+  append_before_action :assert_reset_token_passed, only: :edit
   protected
     def after_resetting_password_path_for(resource)
       thank_you_path
