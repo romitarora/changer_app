01d007573d09e45668c9404607b75a77b2fdeaf8
kinjal Babariya <kinjal@itoneclick.com>
2017-08-01 20:04:58 +0530

    update all data2345

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,16 +22,14 @@ Rails.application.configure do
  config.action_mailer.default_url_options = { :host => '13.58.212.98:3000' }
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  config.serve_static_assets = false
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-
   config.assets.precompile +=  ['*.js','*.js.coffee', '*.css', '*.scss']
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
