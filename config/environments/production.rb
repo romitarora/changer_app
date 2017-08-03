1c511feb8385429b0a964ae6685bd0c88ebab858
kinjal Babariya <kinjal@itoneclick.com>
2017-08-03 18:17:15 +0530

    update all css245

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
  config.action_mailer.default_url_options = { :host => '13.58.212.98:3000' }
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
